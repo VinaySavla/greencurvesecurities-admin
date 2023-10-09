33a193cb800b6befece32c85848673dbbc3bbacf
VinaySavla <savlavinay77@gmail.com>
2023-10-09 21:20:11 +0530

    greencurve contact data showing page

---
+++ src/constants/time.js
@@ -0,0 +1,29 @@
+const SECOND = 1000;
+const SECONDS = SECOND;
+
+const MINUTE = 60 * SECONDS;
+const MINUTES = MINUTE;
+
+const HOUR = 60 * MINUTES;
+const HOURS = HOUR;
+
+const DAY = 24 * HOURS;
+const DAYS = DAY;
+
+const MONTH = 30 * DAYS;
+const MONTHS = MONTH;
+
+const YEAR = 365 * DAYS;
+const YEARS = YEAR;
+
+const API = "https://greencurvesecurities-server.vercel.app/contact";
+
+module.exports = {
+    SECOND, SECONDS,
+    MINUTE, MINUTES,
+    HOUR, HOURS,
+    DAY, DAYS,
+    MONTH, MONTHS,
+    YEAR, YEARS,
+    API
+}
