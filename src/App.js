fad374029a060fe5c64d534db6f829d82d1dc275
VinaySavla <savlavinay77@gmail.com>
2023-10-12 00:12:34 +0530

    auth based DataTable display added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from "./auth/Auth"
-import DataTable from "./components/DataTable"
+// import DataTable from "./components/DataTable"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Auth />} />
-        <Route path="/contacts" element={<DataTable />} />
+        {/* <Route path="/contacts" element={<DataTable />} /> */}
       </Routes>
     </BrowserRouter>
   )
--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Auth.css";
 import { API } from "../constants/time";
 import axios from "axios";
+import DataTable from "../components/DataTable";
 
 export default function Auth(props) {
   // let [authMode, setAuthMode] = useState("signin");
@@ -102,129 +103,133 @@ export default function Auth(props) {
   // }
 
   // if (authMode === "signin") {
-  if (!isLoading) {
-    return (
-      <div className="Auth-form-container">
-        <form className="Auth-form" onSubmit={handleOnSignInSubmit}>
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign In</h3>
-            {/* <div className="text-center">
+  if (isLoggedIn) {
+    return <DataTable />;
+  } else {
+    if (!isLoading) {
+      return (
+        <div className="Auth-form-container">
+          <form className="Auth-form" onSubmit={handleOnSignInSubmit}>
+            <div className="Auth-form-content">
+              <h3 className="Auth-form-title">Sign In</h3>
+              {/* <div className="text-center">
               Not registered yet?{" "}
               <span className="link-primary" onClick={changeAuthMode}>
                 Sign Up
               </span>
             </div> */}
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder="Enter email"
-                name="email"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-                name="password"
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-            {/* <p className="text-center mt-2">
+              <div className="form-group mt-3">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  placeholder="Enter email"
+                  name="email"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Enter password"
+                  name="password"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+              {/* <p className="text-center mt-2">
               Forgot <a href="#">password?</a>
             </p> */}
-          </div>
-        </form>
-      </div>
-    );
-  } else {
-    return (
-      <div class="scene">
-        <div class="shadow"></div>
-        <div class="jumper">
-          <div class="spinner">
-            <div class="scaler">
-              <div class="loader">
-                <div class="cuboid">
-                  <div class="cuboid__side"></div>
-                  <div class="cuboid__side"></div>
-                  <div class="cuboid__side"></div>
-                  <div class="cuboid__side"></div>
-                  <div class="cuboid__side"></div>
-                  <div class="cuboid__side"></div>
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div className="scene">
+          <div className="shadow"></div>
+          <div className="jumper">
+            <div className="spinner">
+              <div className="scaler">
+                <div className="loader">
+                  <div className="cuboid">
+                    <div className="cuboid__side"></div>
+                    <div className="cuboid__side"></div>
+                    <div className="cuboid__side"></div>
+                    <div className="cuboid__side"></div>
+                    <div className="cuboid__side"></div>
+                    <div className="cuboid__side"></div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 
-  // return (
-  //   <div className="Auth-form-container">
-  //     <form className="Auth-form" onSubmit={handleOnSignUpSubmit}>
-  //       <div className="Auth-form-content">
-  //         <h3 className="Auth-form-title">Sign In</h3>
-  //         <div className="text-center">
-  //           Already registered?{" "}
-  //           <span className="link-primary" onClick={changeAuthMode}>
-  //             Sign In
-  //           </span>
-  //         </div>
-  //         <div className="form-group mt-3">
-  //           <label>Full Name</label>
-  //           <input
-  //             type="text"
-  //             className="form-control mt-1"
-  //             placeholder="e.g Jane Doe"
-  //             name="name"
-  //           />
-  //         </div>
-  //         <div className="form-group mt-3">
-  //           <label>Email address</label>
-  //           <input
-  //             type="email"
-  //             className="form-control mt-1"
-  //             placeholder="Email Address"
-  //             name="email"
-  //           />
-  //         </div>
-  //         <div className="form-group mt-3">
-  //           <label>Phone Number</label>
-  //           <input
-  //             type="number"
-  //             className="form-control mt-1"
-  //             placeholder="Phone Number"
-  //             name="phoneNumber"
-  //           />
-  //         </div>
-  //         <div className="form-group mt-3">
-  //           <label>Password</label>
-  //           <input
-  //             type="password"
-  //             className="form-control mt-1"
-  //             placeholder="Password"
-  //             name="password"
-  //           />
-  //         </div>
-  //         <div className="d-grid gap-2 mt-3">
-  //           <button type="submit" className="btn btn-primary">
-  //             Submit
-  //           </button>
-  //         </div>
-  //         {/* <p className="text-center mt-2">
-  //           Forgot <a href="#">password?</a>
-  //         </p> */}
-  //       </div>
-  //     </form>
-  //   </div>
-  // );
+    // return (
+    //   <div className="Auth-form-container">
+    //     <form className="Auth-form" onSubmit={handleOnSignUpSubmit}>
+    //       <div className="Auth-form-content">
+    //         <h3 className="Auth-form-title">Sign In</h3>
+    //         <div className="text-center">
+    //           Already registered?{" "}
+    //           <span className="link-primary" onClick={changeAuthMode}>
+    //             Sign In
+    //           </span>
+    //         </div>
+    //         <div className="form-group mt-3">
+    //           <label>Full Name</label>
+    //           <input
+    //             type="text"
+    //             className="form-control mt-1"
+    //             placeholder="e.g Jane Doe"
+    //             name="name"
+    //           />
+    //         </div>
+    //         <div className="form-group mt-3">
+    //           <label>Email address</label>
+    //           <input
+    //             type="email"
+    //             className="form-control mt-1"
+    //             placeholder="Email Address"
+    //             name="email"
+    //           />
+    //         </div>
+    //         <div className="form-group mt-3">
+    //           <label>Phone Number</label>
+    //           <input
+    //             type="number"
+    //             className="form-control mt-1"
+    //             placeholder="Phone Number"
+    //             name="phoneNumber"
+    //           />
+    //         </div>
+    //         <div className="form-group mt-3">
+    //           <label>Password</label>
+    //           <input
+    //             type="password"
+    //             className="form-control mt-1"
+    //             placeholder="Password"
+    //             name="password"
+    //           />
+    //         </div>
+    //         <div className="d-grid gap-2 mt-3">
+    //           <button type="submit" className="btn btn-primary">
+    //             Submit
+    //           </button>
+    //         </div>
+    //         {/* <p className="text-center mt-2">
+    //           Forgot <a href="#">password?</a>
+    //         </p> */}
+    //       </div>
+    //     </form>
+    //   </div>
+    // );
+  }
 }
